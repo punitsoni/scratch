d0064fe1c1ad26ff4f3a7a12b50245b1b0a7a462
Punit Soni <punitxsmart@gmail.com>
2016-03-07 22:30:10 -0800

    arduino: build system updates

---
+++ arduino/config.mk
@@ -0,0 +1,30 @@
+SERIAL_PORT = /dev/tty.usbserial-A702PYFC
+
+# arduino variant for nano board
+ARD_BOARD_VARIANT = eightanaloginputs
+ARD_SRC_PATH = arduino-src
+
+CC = avr-gcc
+CXX = avr-g++
+AR = avr-ar
+LD = avr-ld
+OBJCOPY = avr-objcopy
+
+ARD_CFLAGS = -Os -DF_CPU=16000000UL -mmcu=atmega328p
+ARD_CFLAGS += -I $(ARD_SRC_PATH)/hardware/arduino/avr/cores/arduino
+ARD_CFLAGS += -I $(ARD_SRC_PATH)/hardware/arduino/avr/variants/$(ARD_BOARD_VARIANT)
+
+#ARD_LDFLAGS = -mmcu=atmega328p
+ARD_LDFLAGS += -L $(PWD)
+
+arduino-src:
+	@echo ">> fetching arduino 1.5.x sources..."
+	wget https://github.com/arduino/Arduino/archive/ide-1.5.x.zip
+	unzip ide-1.5.x.zip
+	mv Arduino-ide-1.5.x $(ARD_SRC_PATH)
+	rm -f ide-1.5.x.zip
+	@echo ">> fetch complete."
+
+# remove fetched sources
+clean-src:
+	rm -rf arduino-src/
---
+++ arduino/libarduino.mk
@@ -0,0 +1,24 @@
+default: libarduino.a
+
+include config.mk
+
+ARD_CORE_PATH = $(ARD_SRC_PATH)/hardware/arduino/avr/cores/arduino
+
+C_SRCS =  $(wildcard $(ARD_CORE_PATH)/*.c)
+CXX_SRCS = $(wildcard $(ARD_CORE_PATH)/*.cpp)
+
+OBJS = $(patsubst %.c,%.o,$(C_SRCS))
+OBJS += $(patsubst %.cpp,%.o,$(CXX_SRCS))
+
+CFLAGS = $(ARD_CFLAGS)
+
+CXXFLAGS = $(CFLAGS)
+
+libarduino.a: $(OBJS)
+	$(AR) rcs libarduino.a $^
+	@echo "DONE."
+
+# remove build artifacts
+clean:
+	rm -f $(OBJS)
+	rm -f libarduino.a
