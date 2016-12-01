b17d51401d2db34b96e2b69a810109e7ada2da47
Punit Soni <punitxsmart@gmail.com>
2016-11-30 17:53:02 -0800

    vagrant-test: updates

--- vagrant-test/vagrant/systemconfig.sh
+++ vagrant-test/vagrant/systemconfig.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
+# This script is run as root user when vagrant machine first comes up
+# or when provision is initiated using "vagrant provision" command
 
 if [ $(id -u) != "0" ]; then
     echo "ERROR: this script must be run as root." >&2
     exit 1
 fi
 
-echo "provision: system configuration"
+echo "[provision] system configuration"
 
 install() {
     echo "installing $1"
     apt-get install -y $1 > /dev/null
 }
 
-echo "updating package database"
+echo "updating package database..."
 apt-get update > /dev/null
-
+echo "upgrading packages..."
+apt-get -y upgrade > /dev/null
+apt-get -y autoremove > /dev/null
 install git vim
 install tmux
 install unzip tree
--- vagrant-test/vagrant/userconfig.sh
+++ vagrant-test/vagrant/userconfig.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
+# This script is run as default user when vagrant machine first comes up
+# or when provision is initiated using "vagrant provision" command
+# This always runs after system configuration is finished
 
-echo "provision: user configuration"
+echo "[provision] user configuration"
 
-echo "HOME=$HOME"
+echo "installing dotfiles..."
+rm -rf /tmp/dotfiles
+git clone https://github.com/punitsoni/dotfiles.git /tmp/dotfiles \
+  2> /dev/null
+/tmp/dotfiles/setup.sh -f > /dev/null
+rm -rf /tmp/dotfiles
 
-cd $HOME
-ln -s /vagrant $HOME/v
+# TODO: move this to a separate file
+# current-user specific settings
+git config --global user.name "Punit Soni"
+git config --global user.email "punitxsmart@gmail.com"
 
-echo "installing dotfiles"
-wget https://github.com/punitsoni/dotfiles/archive/master.zip -P /tmp/ > /dev/null 2>&1
-unzip /tmp/master.zip > /dev/null
-mv dotfiles-master dotfiles
-rm -f /tmp/master.zip
-#cd dotfiles
-#echo "y\ny\ny\n" | ./setup.sh
 echo "DONE."
